7614fefe34abc74c641637c6e31f6d4fa5f28161
Hirp <hirpwastooshort@gmail.com>
2014-12-27 08:16:33 +0100

    Recipes

--- src/main/java/com/hirp/letsmodtutorial/creativetab/CreativeTabLMT.java
+++ src/main/java/com/hirp/letsmodtutorial/creativetab/CreativeTabLMT.java
@@ -7,16 +7,10 @@ import net.minecraft.item.Item;
 
 public class CreativeTabLMT
 {
-    public static final CreativeTabs LMT_TAB = new CreativeTabs(Reference.MOD_ID) {
+    public static final CreativeTabs LMT_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase()) {
         @Override
         public Item getTabIconItem() {
             return ModItems.psiCrystal;
-        }
-
-        @Override
-    public String getTranslatedTabLabel()
-        {
-            return ("Psionic");
         }
     };
 }
